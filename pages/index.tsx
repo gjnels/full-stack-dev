9f09c83cc6a9f1cb158e18d9a7dcf6540af645e9
garrett <gnelson@duck.com>
2022-12-12 00:07:05 -0500

    Added tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <h1>
-      Welcome to <a href="https://themodern.dev">The Modern Dev</a>
-    </h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-center text-xl font-semibold">
+        Welcome to{" "}
+        <a
+          href="https://themodern.dev"
+          className="underline underline-offset-2 text-blue-600 hover:text-blue-500"
+        >
+          The Modern Dev
+        </a>
+      </h1>
+    </div>
   );
 }
