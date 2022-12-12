2c22bc40bd90d5ef872791f9f47c62aa0c44703d
garrett <gnelson@duck.com>
2022-12-12 02:56:35 -0500

    Styled store layout

--- layouts/Store.tsx
+++ layouts/Store.tsx
@@ -1,7 +1,7 @@
+import { PropsWithChildren } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { PropsWithChildren } from "react";
 import {
   companyLogo,
   companyName,
@@ -12,12 +12,16 @@ import {
 } from "../lib/constants";
 
 export interface StoreLayoutProps {
-  title?: String;
+  title?: string;
 }
 
+const Logo = () => (
+  <Image alt={`${companyName} logo`} src={companyLogo} width={40} height={40} />
+);
+
 const StoreLayout = ({
-  children,
-  title,
+  title = "",
+  children = null,
 }: PropsWithChildren<StoreLayoutProps>) => {
   return (
     <>
@@ -25,54 +29,78 @@ const StoreLayout = ({
         <title>{title}</title>
       </Head>
 
-      <div>
-        <header>
-          <Link href="/">
-            <Image
-              src={companyLogo}
-              alt={`${companyName} logo`}
-              width={40}
-              height={40}
-            />{" "}
-            {companyName}
-          </Link>
-
-          <nav>
-            {headerLinks.map((link) => (
-              <Link key={link.label} href={link.href}>
-                {link.label}
+      <div className="flex min-h-screen flex-col">
+        <header className="sticky top-0 z-10 border-b bg-white">
+          <div className="container mx-auto px-4">
+            <div className="flex items-center justify-between py-2">
+              <Link href="/">
+                <Logo />
               </Link>
-            ))}
-          </nav>
+
+              <nav className="hidden space-x-8 sm:block">
+                {headerLinks.map((link) => (
+                  <Link
+                    key={link.label}
+                    href={link.href}
+                    className="font-medium transition hover:text-red-500 hover:underline hover:underline-offset-2"
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          </div>
         </header>
 
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
 
-        <footer>
-          <Link href="/">
-            <Image
-              src={companyLogo}
-              alt={`${companyName} logo`}
-              width={40}
-              height={40}
-            />{" "}
-            {companyName}
-          </Link>
+        <footer className="border-t">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-col gap-12 pt-12 pb-16 lg:flex-row">
+              <div className="space-y-2 lg:max-w-sm lg:flex-1">
+                <Link
+                  href="/"
+                  className="flex flex-col items-center gap-2 lg:flex-row"
+                >
+                  <Logo />
+                  <span className="text-xl font-semibold tracking-wide text-red-600">
+                    {companyName}
+                  </span>
+                </Link>
 
-          <p>{description}</p>
+                <p className="mx-auto max-w-[210px] text-center font-light lg:max-w-none lg:text-left">
+                  {description}
+                </p>
+              </div>
 
-          {footerLinkLists.map((group) => (
-            <ul key={group.groupLabel}>
-              <h3>{group.groupLabel}</h3>
-              {group.links.map((link) => (
-                <li key={link.label}>
-                  <Link href={link.href}>{link.label}</Link>
-                </li>
-              ))}
-            </ul>
-          ))}
+              <div className="grid grid-cols-1 gap-12 text-center md:grid-cols-3 md:text-left lg:flex-1">
+                {footerLinkLists.map((linkList) => (
+                  <ul key={linkList.groupLabel} className="space-y-6">
+                    <h3 className="relative text-xl font-semibold after:absolute after:left-1/2 after:mt-2 after:block after:h-px after:w-8 after:-translate-x-1/2 after:bg-black after:content-[''] md:after:static md:after:translate-x-0">
+                      {linkList.groupLabel}
+                    </h3>
+                    <li className="flex flex-col space-y-2">
+                      {linkList.links.map((link) => (
+                        <Link
+                          key={link.label}
+                          href={link.href}
+                          className="transition hover:text-red-500 hover:underline hover:underline-offset-2"
+                        >
+                          {link.label}
+                        </Link>
+                      ))}
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            </div>
 
-          <span>{getCopyrightText()}</span>
+            <div className="border-t py-6">
+              <p className="text-center text-sm text-gray-500">
+                {getCopyrightText()}
+              </p>
+            </div>
+          </div>
         </footer>
       </div>
     </>
